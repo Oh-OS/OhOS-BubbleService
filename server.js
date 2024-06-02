b8ef52199806c0c467cf685307fdccf1b6312c71
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2024-06-03 07:39:46 +0900

    modify

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
     maxAge: 1000 * 60 * 60
-  }
+    }
 }));
 
 app.use(cors({
@@ -32,83 +32,83 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await prisma.user.findUnique({ where: { email } });
+    try {
+        const { email, password } = req.body;
+        const user = await prisma.user.findUnique({ where: { email } });
 
-    if (user) {
-      if (password === user.password) {
-        req.session.user = user;
-        console.log(req.session.user);
-        return res.status(200).json(user);
-      } else {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-    } else {
-      return res.status(404).json({ error: 'User Not Found' });
+        if (user) {
+            if (password === user.password) {
+                req.session.user = user;
+                console.log(req.session.user);
+                return res.status(200).json(user);
+            } else {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
+        } else {
+            return res.status(404).json({ error: 'User Not Found' });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'login error' });
     }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: 'login error' });
-  }
 });
 
 app.get('/rooms', async (req, res) => {
-  try {
-    const rooms = await prisma.room.findMany();
-    res.json(rooms);
-  } catch (error) {
-    console.error(` 방 불러오기 실패: ${error.message}`);
-    res.status(500).json({ error: '방 목록 불러오기 실패' });
-  }
+    try {
+        const rooms = await prisma.room.findMany();
+        res.json(rooms);
+    } catch (error) {
+        console.error(` 방 불러오기 실패: ${error.message}`);
+        res.status(500).json({ error: '방 목록 불러오기 실패' });
+    }
 });
 
 io.on('connection', (socket) => {
-  console.log('유저 연결됨');
+    console.log('유저 연결됨');
 
-  socket.on('join room', async (roomId) => {
-    try {
-      const room = await prisma.room.findUnique({
-        where: { id: roomId },
-        include: { chats: true },
-      });
+    socket.on('join room', async (roomId) => {
+        try {
+            const room = await prisma.room.findUnique({
+            where: { id: roomId },
+            include: { chats: true },
+        });
 
-      if (!room) {
-        console.error(`${roomId}방은 존재하지 않습니다.`);
-        socket.emit('error', '방이 존재하지 않습니다.');
-        return;
-      }
+        if (!room) {
+            console.error(`${roomId}방은 존재하지 않습니다.`);
+            socket.emit('error', '방이 존재하지 않습니다.');
+            return;
+        }
 
-      socket.join(roomId);
-      socket.emit('room history', room.chats);
-    } catch (error) {
-      console.error(`채팅 및 방 찾기 실패: ${error.message}`);
-      socket.emit('error', '채팅 및 방 찾기 실패');
+        socket.join(roomId);
+        socket.emit('room history', room.chats);
+        } catch (error) {
+            console.error(`채팅 및 방 찾기 실패: ${error.message}`);
+            socket.emit('error', '채팅 및 방 찾기 실패');
+        }
+  });
+
+    socket.on('chat message', async ({ roomId, userId, message }) => {
+        try {
+            const chat = await prisma.chat.create({
+                data: {
+                roomKey: roomId,
+                userKey: userId,
+                chat: message,
+            },
+        });
+        io.to(roomId).emit('chat message', chat);
+        console.log(chat);
+        } catch (error) {
+            console.error(`메세지 보내기 실패: ${error.message}`);
+            socket.emit('error', '메세지 보내기 실패');
     }
   });
 
-  socket.on('chat message', async ({ roomId, userId, message }) => {
-    try {
-      const chat = await prisma.chat.create({
-        data: {
-          roomKey: roomId,
-          userKey: userId,
-          chat: message,
-        },
-      });
-      io.to(roomId).emit('chat message', chat);
-      console.log(chat);
-    } catch (error) {
-      console.error(`메세지 보내기 실패: ${error.message}`);
-      socket.emit('error', '메세지 보내기 실패');
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
