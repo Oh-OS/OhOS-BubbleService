c5ad27f04564c3865753571aa3ff4349db400b0a
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2024-06-18 07:46:03 +0900

    session 완전히 삭제 및 불필요한 코드 제거

--- server.js
+++ server.js
@@ -54,30 +54,6 @@ app.get('/getUserInfo/:id', async (req, res) => {
     }
 })
 
-// 로그인
-app.post('/login', async (req,res) => {
-    try {
-        const {email, password} = req.body;
-
-        const user = await prisma.user.findUnique({ where: {email} });
-
-        if(user){
-            if(password === user.password){
-                req.session.user = user;
-
-                console.log(req.session.user);
-                return res.status(200).json(user);
-            }else{
-                return res.status(401).json({ error : 'Unauthorized'})
-            }
-        }else {
-            return res.status(404).json({ error : 'User Not Found'})
-        }
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ error : 'login error '})
-    }
-})
 // 각 방의 최근 메세지 가져오기
 app.get('/recent-messages', async (req, res) => {
     try {
@@ -112,7 +88,6 @@ app.get('/recent-messages', async (req, res) => {
 // 채팅관련
 io.on('connection', (socket) => {
     console.log('유저 연결됨');
-    //console.log(req.session);
 
     // 채팅방에 참가 
     socket.on('join room', async (roomId) => {
@@ -137,19 +112,12 @@ io.on('connection', (socket) => {
     });
 
     // 채팅 메세지 전송
-    socket.on('chat message', async ({ roomId, message }) => {
+    socket.on('chat message', async ({ roomId, userId, message }) => {
         try {
-
-            const user = socket.request.session.user;
-            if (!user) {
-                socket.emit('error', 'User not authenticated');
-                return;
-            }
-            console.log(user);
             const chat = await prisma.chat.create({
                 data: {
                 roomKey: roomId,
-                userKey: user.id,
+                userKey: userId,
                 chat: message,
             },
         });
